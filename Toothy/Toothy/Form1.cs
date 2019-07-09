8678b9f9db7ec7c7b82009420763e62d519abd73
Chad <lickey10@gmail.com>
2019-07-09 14:37:21 -0600

    Initial commit

---
+++ Toothy/Toothy/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using InTheHand.Net.Sockets;
+
+namespace Toothy
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void getDevices()
+        {
+            BluetoothClient client = new BluetoothClient();
+            List<string> items = new List<string>();
+            BluetoothDeviceInfo[] devices = client.DiscoverDevicesInRange();
+            foreach (BluetoothDeviceInfo d in devices)
+            {
+                items.Add(d.DeviceName);
+            }
+        }
+    }
+}
